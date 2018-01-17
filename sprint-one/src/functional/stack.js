658d6e240bbd3940828c37b891f51b7ea64ef0ce
Becca Stanger <beccastanger@gmail.com>
2018-01-17 11:10:08 -0800

    Add changes discussed in lecture
    Pass first few tests for size of stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var sizeCount = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    sizeCount++;
   };
 
   someInstance.pop = function() {
+    sizeCount--;
   };
 
   someInstance.size = function() {
+    return sizeCount;
   };
 
   return someInstance;
