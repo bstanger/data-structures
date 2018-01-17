17055397f37c3c27363816133469af80ac6d3314
Becca Stanger <beccastanger@gmail.com>
2018-01-17 11:46:57 -0800

    Pass all remaining stack shared tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -8,10 +8,28 @@ var Stack = function() {
   // Implement the methods below
   someInstance.push = function(value) {
     sizeCount++;
+    storage[0] = value;
+    // Need adjust all others in storage
+    
+    // var nextValue;
+    // for (var idxKeys in storage){
+    //   console.log('looking at index ' , idxKeys);
+    //   console.log('with value ' , storage[idxKeys]);
+    //   nextValue = storage[idxKeys++];
+    //   console.log('nextValue ' , nextValue);
+    //   storage[idxKeys++] = storage[idxKeys];
+    //   console.log('updated value ' , storage[idxKeys]);
+    // }
+    return sizeCount;
   };
 
   someInstance.pop = function() {
+    if(!sizeCount) return;
+    var valuePopped = storage[0];
+    delete storage[0];
     sizeCount--;
+    // Need adjust all others in storage
+    return valuePopped;
   };
 
   someInstance.size = function() {
