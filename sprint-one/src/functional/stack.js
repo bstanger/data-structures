a9117d3bd456ed222e50c9bbd68873fe88ede81d
Becca Stanger <beccastanger@gmail.com>
2018-01-17 18:44:29 -0800

    Refactor functional stack to use a storage obj

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,33 +1,39 @@
 var Stack = function() {
   var someInstance = {};
-  someInstance.top = null;
+  someInstance.topIdx = -1;
   someInstance.sizeCount = 0;
 
   // Use an object with numeric keys to store values
   // Note: time permitting, I should go back and use the numeric keys.  I misunderstood that before.
-  var inputObj = {};
+  var storage = {};
+  var index = 0;
 
   var newInputObj = function(value){
     var output = new Object();
     output.value = value;
-    output.previous = null;
+    output.previousIdx = null;
     return output;
   };
 
   someInstance.push = function(value) {
     var inputObj = new newInputObj(value);
-    inputObj.previous = someInstance.top;
-    someInstance.top = inputObj;
+    inputObj.previousIdx = someInstance.topIdx;
+    someInstance.topIdx = index;
+
+    storage[index] = inputObj;
+
+    index++;
     someInstance.sizeCount++;
+
     return someInstance.sizeCount;
   };
 
   someInstance.pop = function() {
     if(!someInstance.sizeCount) return;
-    var removed = someInstance.top.value;
-    someInstance.top = someInstance.top.previous;
+    var topObj = storage[someInstance.topIdx];
+    someInstance.topIdx = topObj.previousIdx;
     someInstance.sizeCount--;
-    return removed;
+    return topObj.value;
   };
 
   someInstance.size = function() {
