82fc7e951a4232d76c310fc791839048f8f8f9bd
Becca Stanger <beccastanger@gmail.com>
2018-01-17 21:58:26 -0800

    Implement functional-shared for stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,52 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var currentStack = {
+  	sizeCount: 0, 
+  	topIdx: -1,
+  	storage: {},
+  	index: 0
+  };
+
+  _.extend(currentStack, stackMethods);
+
+  return currentStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+	newInputObj: function(value){
+		var newObj = {};
+		newObj.value = value;
+    	newObj.previousIdx = null;
+    	return newObj;
+	},
+
+	push: function(value){
+		var newInputObj = this.newInputObj(value);
+	    newInputObj.previousIdx = this.topIdx;
+	    this.topIdx = this.index;
+
+	    this.storage[this.index] = newInputObj;
+
+	    this.index++;
+	    this.sizeCount++;
+
+	    return this.sizeCount;
+	},
+
+	pop: function(){
+		if(!this.sizeCount) return;
+	    var topObj = this.storage[this.topIdx];
+	    this.topIdx = topObj.previousIdx;
+	    this.sizeCount--;
+	    return topObj.value;
+	},
+
+	size: function() {
+		return this.sizeCount;
+	}
+};
 
 
