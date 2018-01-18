902b9720d2c9688326ab0dcfc7edd4890eac6afd
Becca Stanger <beccastanger@gmail.com>
2018-01-17 17:59:49 -0800

    Fix functional-shared to include enqueue and deq

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,6 +3,10 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var currentQueue = {};
   currentQueue.sizeCount = 0;
+  currentQueue.firstIdx = 0;
+  currentQueue.storage = {};
+  currentQueue.index = 0;
+  
   _.extend(currentQueue, queueMethods);
 
   return currentQueue;
@@ -10,12 +14,32 @@ var Queue = function() {
 };
 
 var queueMethods = {
-	// createQueueObj: function(value){
-	//     var queueObj = {};
-	//     queueObj.value = value;
-	//     queueObj.previous = null;
-	//     return queueObj;
-	// },
+	createQueueObj: function(value){
+	    var queueObj = {};
+	    queueObj.value = value;
+	    return queueObj;
+	},
+
+	enqueue: function(value){
+		var freshQItem = this.createQueueObj(value);
+		this.storage[this.index] = freshQItem;
+
+		this.index++;
+		this.sizeCount++;
+
+		return;
+	},
+
+	dequeue: function(){
+		if(!this.sizeCount) return;
+		var dequeuedValue = this.storage[this.firstIdx].value;
+		delete this.storage[this.firstIdx];
+
+		this.firstIdx++;
+		this.sizeCount--;
+
+		return dequeuedValue;
+	},
 
 	size: function(){
 		return this.sizeCount;
